610768fd4f6747e6677629ea8f851053a4c4878f
superernie77 <superernie77@gmail.com>
2018-07-27 11:47:49 +0200

    blank project

---
+++ src/main/java/com/se77/reactivemongodb/dome/Application.java
@@ -0,0 +1,12 @@
+package com.se77.reactivemongodb.dome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
